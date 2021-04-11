ba9ff345665bf975bf7e1fd0ac9ee0aa9fd4308b
DegtyarevNick <nikolay.degtyarev@digexco.com>
2021-04-12 01:34:44 +0300

    fix converting minutes problem

--- Chapter 1 exer#1.js
+++ Chapter 1 exer#1.js
@@ -13,6 +13,7 @@ function scheduleMeeting(startTime, durationMinutes) {
       }
       return NaN;
     });
+    console.log(time);
   if (
     !time.includes(NaN) &&
     time.length === 2 &&
@@ -23,12 +24,13 @@ function scheduleMeeting(startTime, durationMinutes) {
   ) {
     if (time[1] + durationMinutes >= 60) {
       time[0] += Math.floor((time[1] + durationMinutes) / 60);
-      time[1] = time[1] + durationMinutes - 60 * Math.floor((time[1] + durationMinutes) / 60) == 0
-          ? "00"
-          : time[1] + durationMinutes - 60 * Math.floor((time[1] + durationMinutes) / 60); // converting minutes for comparison(0 => 00)
+      time[1] = String(time[1] + durationMinutes - 60 * Math.floor((time[1] + durationMinutes) / 60)).length === 1
+          ? "0"+ String(time[1] + durationMinutes - 60 * Math.floor((time[1] + durationMinutes) / 60))
+          : time[1] + durationMinutes - 60 * Math.floor((time[1] + durationMinutes) / 60); // converting minutes for comparison(0 => 00) etc
     } else {
       time[1] += durationMinutes;
     }
+    console.log(time)
     return (
       Number(time.join("")) >= Number(dayStart.replace(":", "")) &&
       Number(time.join("")) <= Number(dayEnd.replace(":", ""))
@@ -40,7 +42,7 @@ function scheduleMeeting(startTime, durationMinutes) {
 console.log(scheduleMeeting("7:00", 15));
 console.log(scheduleMeeting("07:15", 30));
 console.log(scheduleMeeting("7:30", 30));
-console.log(scheduleMeeting("11:30", 60));
+console.log(scheduleMeeting("11:30", 36));
 console.log(scheduleMeeting("17:00", 45));
 console.log(scheduleMeeting("17:30", 30));
 console.log(scheduleMeeting("18:00", 15));
